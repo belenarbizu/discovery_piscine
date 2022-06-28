2f816432df21a0bac25f220919327988fd43894a
belenarbizu <100694123+belenarbizu@users.noreply.github.com>
2022-06-28 09:51:18 +0200

    Add files via upload

---
+++ cell3/ex00/background.js
@@ -0,0 +1,12 @@
+function myFunction()
+{
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+}
